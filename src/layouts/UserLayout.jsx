2770de8c404325614b53692d2fcd3491438683b7
zhangxiang <betterzx153@gmail.com>
2019-12-04 11:20:07 +0800

    save

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -38,7 +38,7 @@ const UserLayout = props => {
             <div className={styles.header}>
               <Link to="/">
                 <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>西南石油大学开放性实验管理系统</span>
+                <span className={styles.title}>西南石油大学开放实验管理系统</span>
               </Link>
             </div>
             {/* <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div> */}
